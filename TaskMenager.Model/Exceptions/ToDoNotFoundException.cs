43f6851ce50b113ad8bf3a6953a39cef84c13070
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-04-18 10:42:14 +0200

    Added custom exception.

---
+++ TaskMenager.Model/Exceptions/ToDoNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMenager.Domain.Exceptions
+{
+    public class ToDoNotFoundException : Exception
+    {
+        public ToDoNotFoundException() : base("Nie znaleziono zadania w bazie danych.") { }
+    }
+}
